61ee5b999acddeff90614a678910de3a43e806a7
李佳成 <2713554182@qq.com>
2023-03-05 16:40:43 +0800

    EDIT 捕捉then方法错误

--- 11. 实现Promise.js
+++ 11. 实现Promise.js
@@ -37,7 +37,7 @@ class MyPromise {
     if (this.status === PENDING) {
       this.status = REJECTED;
       this.reason = reason;
-      this.onRejectedCallback?.(reason);
+      
       while (this.onRejectedCallback.length) {
         this.onRejectedCallback?.shift()(reason);
       }
@@ -47,8 +47,14 @@ class MyPromise {
   then(onFulfilled, onRejected) {
     const promise2 = new MyPromise((resolve, reject) => {
       if (this.status === FULFILLED) {
-        const result = onFulfilled(this.value);
-        resolvePromise(promise2, result, resolve, reject);
+        queueMicrotask(() => {
+          try {
+            const result = onFulfilled(this.value);
+            resolvePromise(promise2, result, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        });
       } else if (this.status === REJECTED) {
         onRejected(this.reason);
       } else if (this.status === PENDING) {
@@ -74,9 +80,29 @@ function resolvePromise(promise, result, resolve, reject) {
 
 const promise = new MyPromise((resolve, reject) => {
   resolve('success');
+  // throw new Error('执行器错误')
 });
 
-const p1 = promise.then((value) => {
-  console.log('resolve', value);
-  return p1;
-});
+// 第一个then方法中的错误要在第二个then方法中捕获到
+promise
+  .then(
+    (value) => {
+      console.log(1);
+      console.log('resolve', value);
+      throw new Error('then error');
+    },
+    (reason) => {
+      console.log(2);
+      console.log(reason.message);
+    }
+  )
+  .then(
+    (value) => {
+      console.log(3);
+      console.log(value);
+    },
+    (reason) => {
+      console.log(4);
+      console.log(reason.message);
+    }
+  );
