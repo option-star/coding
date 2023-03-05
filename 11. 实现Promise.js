36d99fa361ba808ebc1412b7a3958b7d9c36b7a3
李佳成 <2713554182@qq.com>
2023-03-05 18:34:37 +0800

    EDIT promise简单版实现

--- 11. 实现Promise.js
+++ 11. 实现Promise.js
@@ -1,10 +1,9 @@
 const PENDING = 'pending';
-const REJECTED = 'rejected';
 const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
 
-class MyPromise {
+class Promise {
   constructor(executor) {
-    // executor执行器，进入会立即执行
     try {
       executor(this.resolve, this.reject);
     } catch (error) {
@@ -21,122 +20,75 @@ class MyPromise {
   onRejectedCallback = [];
 
   resolve = (value) => {
-    // 只有PENDING状态才会进行状态修改
     if (this.status === PENDING) {
       this.status = FULFILLED;
       this.value = value;
 
       while (this.onFulfilledCallback.length) {
-        this.onFulfilledCallback?.shift()(value);
+        this.onFulfilledCallback.shift()(value);
       }
     }
   };
 
   reject = (reason) => {
-    // 只有PENDING才会执行状态修改
     if (this.status === PENDING) {
       this.status = REJECTED;
       this.reason = reason;
 
       while (this.onRejectedCallback.length) {
-        this.onRejectedCallback?.shift()(reason);
+        this.onRejectedCallback.shift()(reason);
       }
     }
   };
 
-  then(onFulfilled, onRejected) {
-    // 如果 onFulfilled或者onRejected不传
-    onFulfilled =
-      typeof onFulfilled === 'function' ? onFulfilled : (value) => value;
-    onRejected =
-      typeof onRejected === 'function'
-        ? onRejected
-        : (reason) => {
-            throw reason;
-          };
+  then (onFulfilled, onRejected) {
+    const realOnFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+    const realOnRejected = typeof onRejected ==== ' function' ? onRejected : reason => {throw reason};
 
-    const promise2 = new MyPromise((resolve, reject) => {
+    const promise2 = new Promise((resolve, reject) => {
+
+      const fulfilledMicroTask = queueMicrotask(() => {
+        try {
+          const result = realOnFulfilled(this.value);
+          resolvePromise(promise2, result, resolve, reject)
+        } catch(error) {
+          reject(error)
+        }
+      })
+
+      const rejectMicroTask = queueMicrotask(() => {
+        try {
+          const result = realOnRejected(this.reason);
+          resolvePromise(promise2, result, resolve, reject);
+        } catch (error) {
+          reject(error)
+        }
+      })
+
       if (this.status === FULFILLED) {
-        queueMicrotask(() => {
-          try {
-            const result = onFulfilled(this.value);
-            resolvePromise(promise2, result, resolve, reject);
-          } catch (error) {
-            reject(error);
-          }
-        });
+        fulfilledMicroTask();
       } else if (this.status === REJECTED) {
-        queueMicrotask(() => {
-          try {
-            const result = onRejected(this.reason);
-          } catch (error) {
-            reject(error);
-          }
-        });
+        rejectMicroTask();
       } else if (this.status === PENDING) {
-        this.onFulfilledCallback.push(
-          queueMicrotask(() => {
-            try {
-              const result = onFulfilled(this.value);
-              resolvePromise(result);
-            } catch (error) {
-              reject(error);
-            }
-          })
-        );
-        this.onRejectedCallback.push(
-          queueMicrotask(() => {
-            try {
-              const result = onRejected(this.reason);
-              resolvePromise(result);
-            } catch (error) {
-              reject(error);
-            }
-          })
-        );
-      }
-    });
+        this.onFulfilledCallback.push(fulfilledMicroTask);
+        this.onRejectedCallback.push(rejectMicroTask);
+          }
+
+    })
 
     return promise2;
   }
 }
 
-function resolvePromise(promise, result, resolve, reject) {
-  if (promise === result) {
-    return reject(new TypeError('循环引用自身'));
-  }
-  if (result instanceof MyPromise) {
+function resolvePromise (promise2, result, resolve, reject) {
+  if (promise2 === result) {
+    reject(new TypeError('不可循环引用'))
+  } 
+
+  if (result instanceof Promise) {
     result.then(resolve, reject);
   } else {
     resolve(result);
   }
+
 }
-
-const promise = new MyPromise((resolve, reject) => {
-  resolve('success');
-  // throw new Error('执行器错误')
-});
-
-// 第一个then方法中的错误要在第二个then方法中捕获到
-promise
-  .then(
-    (value) => {
-      console.log(1);
-      console.log('resolve', value);
-      throw new Error('then error');
-    },
-    (reason) => {
-      console.log(2);
-      console.log(reason.message);
-    }
-  )
-  .then(
-    (value) => {
-      console.log(3);
-      console.log(value);
-    },
-    (reason) => {
-      console.log(4);
-      console.log(reason.message);
-    }
-  );
