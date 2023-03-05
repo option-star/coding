90127ee1cb1d601fb2a15ed1e356c317b1ccc151
李佳成 <2713554182@qq.com>
2023-03-05 15:46:09 +0800

    EDIT 状态与结果管理

--- 11. 实现Promise.js
+++ 11. 实现Promise.js
@@ -1,10 +1,31 @@
+const PENDING = 'pending';
+const REJECTED = 'rejected';
+const FULFILLED = 'fulfilled';
+
 class Promise {
   constructor(executor) {
     // executor执行器，进入会立即执行
     executor(this.resolve, this.reject);
   }
 
-  resolve = () => {};
+  status = PENDING;
 
-  reject = () => {};
+  value = null;
+  reason = null;
+
+  resolve = (value) => {
+    // 只有PENDING状态才会进行状态修改
+    if (this.status === PENDING) {
+      this.status = FULFILLED;
+      this.value = value;
+    }
+  };
+
+  reject = (reason) => {
+    // 只有PENDING才会执行状态修改
+    if (this.status === PENDING) {
+      this.status = REJECTED;
+      this.reason = reason;
+    }
+  };
 }
