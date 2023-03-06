b32d203e09fb7d977c1499a87d765237ac1d5c2d
李佳成 <2713554182@qq.com>
2023-03-06 14:26:52 +0800

    EDIT 实现基本版深拷贝

--- 20.深拷贝.js
+++ 20.深拷贝.js
@@ -1,21 +1,45 @@
-const clone = (target, map = new Map()) => {
-  if (typeof target === 'object') {
-    // 处理对象类型与数组类型
-    const cloneTarget = Array.isArray(target) ? [] : {};
+const isObject = (target) =>
+  (typeof target === 'object' || typeof target === 'function') &&
+  target !== null;
 
-    // 处理循环引用
-    if (map.has(target)) return map.get(target);
-    map.set(target, cloneTarget);
+const Clone = (target, map = new WeakMap()) => {
+  // 处理Map
+  if (target instanceof Map) return new Map([...target]);
 
-    for (const key in target) {
-      cloneTarget[key] = clone(target[key], map);
-    }
-    return cloneTarget;
-  } else {
-    return target;
-  }
+  // 处理Set
+  if (target instanceof Set) return new Set([...target]);
+
+  // 处理symbol(值)
+  if (typeof target === 'symbol') return Symbol(target.description);
+
+  // 处理函数
+  if (typeof target === 'function') return target;
+
+  // 处理普通类型
+  if (!isObject(target)) return target;
+
+  // 处理数组
+  const _target = Array.isArray(target) ? [] : {};
+
+  // 处理循环引用
+  if (map.has(target)) return map.get(target);
+  map.set(target, _target);
+
+  Object.keys(target).reduce((pre, cur) => {
+    pre[cur] = Clone(target[cur]);
+    return pre;
+  }, _target);
+
+  return _target;
 };
 
+const map = new Map();
+map.set('key', 'value');
+map.set('Li', 'code秘密花园');
+
+const set = new Set();
+set.add('Li');
+set.add('code秘密花园');
 const target = {
   field1: 1,
   field2: undefined,
@@ -23,9 +47,25 @@ const target = {
     child: 'child',
   },
   field4: [2, 4, 8],
+  empty: null,
+  map,
+  set,
+  bool: new Boolean(true),
+  num: new Number(2),
+  str: new String(2),
+  symbol: Symbol(1),
+  date: new Date(),
+  reg: /\d+/,
+  error: new Error(),
+  func1: () => {
+    console.log('code秘密花园');
+  },
+  func2: function (a, b) {
+    return a + b;
+  },
 };
-target.target = target;
 
-const result = clone(target);
+const result = Clone(target);
 console.log(result);
-console.log(result === target);
+console.log(target);
+console.log(target === result);
